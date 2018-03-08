bd92c24da4578d590fe3eb3f19b9ba914b760604
artfer <artureco95.af@gmail.com>
2018-03-08 20:50:12 +0000

    documentation on top

--- RoboProject/src/man/TikiTho.java
+++ RoboProject/src/man/TikiTho.java
@@ -1,3 +1,11 @@
+/**
+ * Robot that stays in a wall and tries to kill the enemy
+ * by changing between predictive shooting and normal shooting
+ * 
+ * @author up201302914 up201308089
+ * @version 1.2
+ */
+
 package man;
 
 import robocode.*;
@@ -8,40 +16,39 @@ public class TikiTho extends AdvancedRobot {
 	double direction = 1;							//-1 to go back,+1 go ahead
 	double enemyEnergy;								
 	
-	
 	boolean predictLocation=false;					//type of shoots the robot will use
 	double directShots=1,directShotsHit=1;			//how many did the robot shoot and hit
 	double predictiveShots=1,predictiveShotsHit=1;	
 	
 	public void run() {
+		setAdjustRadarForGunTurn(true); //free the radar from the gun	
+		
 		double width  = getBattleFieldWidth();		
 		double height = getBattleFieldHeight();		
-		
-		setAdjustRadarForRobotTurn(true);			//free the radar from the gun
-		setAdjustRadarForGunTurn(true);				
-		
+					
 		while(true) {
 		if(!hitWall) {
 			turnLeft(getHeading() % 90);			//turn to a wall
 			ahead(Math.max(height,width)); 			//and go to that wall
 		}
-			turnRadarLeft(Double.MAX_VALUE);		//spins until finds another robot
+			turnRadarLeft(Double.MAX_VALUE);		//spin radar until finding another robot
 		}
 	}
 	 
+	
 	public void onScannedRobot(ScannedRobotEvent e) {
-		double absBearing = e.getBearingRadians() + getHeadingRadians();		      	//enymys angle relative to our position
+		double absBearing = e.getBearingRadians() + getHeadingRadians();		      	//enemy's angle relative to our position
 		double latVel = e.getVelocity() * Math.sin(e.getHeadingRadians() - absBearing); //lateral velocity of the enemy
 		double radarTurn = absBearing - getRadarHeadingRadians(); 						//angle to turn the radar
 		
-		if(enemyEnergy > (enemyEnergy = e.getEnergy()))							 		//if the enemys energy is lower than before
-			direction*=-1;																//he shoot,change direction
+		if(enemyEnergy > (enemyEnergy = e.getEnergy()))							 		//if the enemy's energy is lower than before
+			direction*=-1;																//he shoot a bullet,change direction
 		
 		if(e.getDistance()<75)															//if he is too close shoot a powerful bullet
 			setFire(3);
 																						
-		double bulletPower=Math.min(2, enemyEnergy/4);								//lets minimize our energy
-																						//bullet power changes with enemys energy
+		double bulletPower=Math.min(2, enemyEnergy/4);									//lets minimize our energy
+																						//bullet power changes with enemy's energy
 		
 		double predictiveRating = predictiveShotsHit/predictiveShots;					//calculate the rating of the two strategies
 		double directRating = directShotsHit/directShots;								
@@ -50,7 +57,7 @@ public class TikiTho extends AdvancedRobot {
 			predictLocation=true;					//shoot to the predicted location
 		else										
 			predictLocation=false;					//shoot directly at the enemy
-		if(predictLocation) {//calculate how much to turn the radar
+		if(predictLocation) {//calculate how much to turn the gun
 			setTurnGunRightRadians(Utils.normalRelativeAngle(absBearing-getGunHeadingRadians()+Math.asin(latVel/(20-3*bulletPower))));
 			predictiveShots++; 						
 		}else {
@@ -58,9 +65,10 @@ public class TikiTho extends AdvancedRobot {
 			directShots++;      					
 		}
 		setFire(bulletPower);//fire!
-		setAhead(100*direction);//dont stop moving					
+		setAhead(100*direction);//don't stop moving					
 		setTurnRadarRightRadians(Utils.normalRelativeAngle(radarTurn)*2); 	//keeps the radar on target		 
 	}
+	
 	
 	
 	public void onHitWall(HitWallEvent e) { //if we hit a wall 
@@ -74,7 +82,6 @@ public class TikiTho extends AdvancedRobot {
 	public void onHitRobot(HitRobotEvent e) {		//if we hit another robot, go the other way
 		direction*=-1;								
 	}												
-	
 	
 	public void onBulletHit(BulletHitEvent e) {		//if we hit the enemy with a bullet
 		if(predictLocation)							//increment the "hit" variables
