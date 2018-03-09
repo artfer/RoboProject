fcf1f41a398c223ee10d793f64e9c36933465962
artfer <artureco95.af@gmail.com>
2018-03-09 18:58:37 +0000

    comment added in fashion and manners

--- RoboProject/src/man/TikiTho.java
+++ RoboProject/src/man/TikiTho.java
@@ -12,16 +12,16 @@ import robocode.*;
 import robocode.util.Utils;
 
 public class TikiTho extends AdvancedRobot {
-	boolean hitWall = false; 						//have we hit a wall yet
+	boolean hitWall = false; 						//have we hit a wall yet?
 	double direction = 1;							//-1 to go back,+1 go ahead
 	double enemyEnergy;								
 	
-	boolean predictLocation=false;					//type of shoots the robot will use
-	double directShots=1,directShotsHit=1;			//how many did the robot shoot and hit
+	boolean predictLocation=false;					//type of shots the robot will use
+	double directShots=1,directShotsHit=1;			//how many shots did the robot hit
 	double predictiveShots=1,predictiveShotsHit=1;	
 	
 	public void run() {
-		setAdjustRadarForGunTurn(true); //free the radar from the gun	
+		setAdjustRadarForGunTurn(true); 			//free the radar from the guns movement
 		
 		double width  = getBattleFieldWidth();		
 		double height = getBattleFieldHeight();		
@@ -31,60 +31,73 @@ public class TikiTho extends AdvancedRobot {
 			turnLeft(getHeading() % 90);			//turn to a wall
 			ahead(Math.max(height,width)); 			//and go to that wall
 		}
-			turnRadarLeft(Double.MAX_VALUE);		//spin radar until finding another robot
+			turnRadarLeft(Double.MAX_VALUE);		//spin radar until it finds the enemy
 		}
 	}
-	 
 	
+	/**
+	 * Using some calculations, we switch between shooting directly at the enemy position
+	 * and shooting at the predicted location associated with it's movement, using a rating system.
+	 * Adding to this we set the robot to shift direction whenever the enemy shoots at us!
+	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
-		double absBearing = e.getBearingRadians() + getHeadingRadians();		      	//enemy's angle relative to our position
-		double latVel = e.getVelocity() * Math.sin(e.getHeadingRadians() - absBearing); //lateral velocity of the enemy
-		double radarTurn = absBearing - getRadarHeadingRadians(); 						//angle to turn the radar
+		double absBearing = e.getBearingRadians() + getHeadingRadians();			//enemy's angle relative to our position
+		double latVel = e.getVelocity()*Math.sin(e.getHeadingRadians()-absBearing); //lateral velocity of the enemy
+		double radarTurn = absBearing - getRadarHeadingRadians(); 					//angle to turn the radar
 		
-		if(enemyEnergy > (enemyEnergy = e.getEnergy()))							 		//if the enemy's energy is lower than before
-			direction*=-1;																//he shoot a bullet,change direction
+		if(enemyEnergy > (enemyEnergy = e.getEnergy())) //the enemy shot at us
+			direction*=-1;
 		
-		if(e.getDistance()<75)															//if he is too close shoot a powerful bullet
+		if(e.getDistance()<75)	//he is close fire a powerful bullet
 			setFire(3);
 																						
-		double bulletPower=Math.min(2, enemyEnergy/4);									//lets minimize our energy
-																						//bullet power changes with enemy's energy
+		double bulletPower=Math.min(2, enemyEnergy/4);	
+		double predictiveRating = predictiveShotsHit/predictiveShots;
+		double directRating = directShotsHit/directShots;
 		
-		double predictiveRating = predictiveShotsHit/predictiveShots;					//calculate the rating of the two strategies
-		double directRating = directShotsHit/directShots;								
-		
-		if(predictiveRating > directRating)			//use the highest rating
-			predictLocation=true;					//shoot to the predicted location
+		if(predictiveRating > directRating)
+			predictLocation=true;				
 		else										
-			predictLocation=false;					//shoot directly at the enemy
-		if(predictLocation) {//calculate how much to turn the gun
+			predictLocation=false;				
+		
+		//calculate how much to turn the gun
+		if(predictLocation) {	
 			setTurnGunRightRadians(Utils.normalRelativeAngle(absBearing-getGunHeadingRadians()+Math.asin(latVel/(20-3*bulletPower))));
 			predictiveShots++; 						
 		}else {
 			setTurnGunRightRadians(Utils.normalRelativeAngle(absBearing-getGunHeadingRadians()));
 			directShots++;      					
 		}
-		setFire(bulletPower);//fire!
-		setAhead(100*direction);//don't stop moving					
+		setFire(bulletPower);
+		setAhead(100*direction);		
 		setTurnRadarRightRadians(Utils.normalRelativeAngle(radarTurn)*2); 	//keeps the radar on target		 
 	}
 	
-	
-	
-	public void onHitWall(HitWallEvent e) { //if we hit a wall 
-		if(hitWall)//change direction									
+	/**
+	 * If we hit a wall,go back
+	 * If it's the beginning of the game stick to the wall
+	 */
+	public void onHitWall(HitWallEvent e) {
+		if(hitWall)								
 			direction*=-1;							
-		else //if its the first time we hit a wall , just turn left
+		else
 			turnLeft(90);							
 		hitWall=true;		
 	}
 	
-	public void onHitRobot(HitRobotEvent e) {		//if we hit another robot, go the other way
+	/**
+	 *If we hit another robot,change direction
+	 */
+	public void onHitRobot(HitRobotEvent e) {		
 		direction*=-1;								
 	}												
 	
-	public void onBulletHit(BulletHitEvent e) {		//if we hit the enemy with a bullet
-		if(predictLocation)							//increment the "hit" variables
+	/**
+	 * If we hit the enemy with a bullet
+	 * increment the hit variables
+	 */
+	public void onBulletHit(BulletHitEvent e) {	
+		if(predictLocation)				
 			predictiveShotsHit++;
 		else
 			directShotsHit++;
